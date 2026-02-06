f1eae8a96ee4bb9c23dbc31bf7f261e6dc01215e
Jeffrey Yu <92849+wehjin@users.noreply.github.com>
2026-02-06 12:47:26 -0800

    Adds state for each of the lot fields into the lot editor.

--- src/views/home/session/lots.rs
+++ src/views/home/session/lots.rs
@@ -6,16 +6,17 @@ struct Editor {
     pub account: String,
     pub time: String,
     pub products: Vec<String>,
-    pub product: usize,
+    pub product: String,
     pub quantity: String,
 }
 impl Editor {
     pub fn new(products: Vec<String>) -> Self {
+        let product = products[0].clone();
         Self {
             account: "".to_string(),
             time: "".to_string(),
             products,
-            product: 0,
+            product,
             quantity: "".to_string(),
         }
     }
@@ -56,6 +57,24 @@ enum Ending {
 
 #[component]
 fn EditLot(editor: Editor, on_end: EventHandler<Ending>) -> Element {
+    let mut account_state = use_signal(|| editor.account);
+    let mut time_state = use_signal(|| editor.time);
+    let mut product_state = use_signal(|| editor.product);
+    let mut quantity_state = use_signal(|| editor.quantity);
+    let mut valid = use_signal(|| "".to_string());
+    let products = editor.products.clone();
+    use_effect(move || {
+        let product = product_state();
+        let account = account_state();
+        let time = time_state();
+        let quantity = quantity_state();
+        let desc = format!(
+            "Account: {}, Time: {}, Product: {}, Quantity: {}",
+            account, time, product, quantity
+        );
+        valid.set(desc);
+    });
+    let products = editor.products.clone();
     rsx! {
         div { class: "modal is-active",
             div { class: "modal-background" }
@@ -65,16 +84,17 @@ fn EditLot(editor: Editor, on_end: EventHandler<Ending>) -> Element {
                     button { class: "delete", aria_label: "close", onclick: move |_| on_end.call(Ending::Cancel) }
                 }
                 section { class: "modal-card-body",
+                    p { class: "has-text-grey", "{valid}" }
                     div { class: "field",
                         label { class: "label", "Account" }
                         div { class: "control",
-                            input { class:"input", type: "text" }
+                            input { class:"input", type: "text", oninput: move |e| account_state.set(e.value()) }
                         }
                     }
                     div { class: "field",
                         label { class: "label", "Time" }
                         div { class: "control",
-                            input { class:"input", type: "text" }
+                            input { class:"input", type: "text", oninput: move |e| time_state.set(e.value()) }
                         }
                     }
                     div { class: "columns",
@@ -83,8 +103,13 @@ fn EditLot(editor: Editor, on_end: EventHandler<Ending>) -> Element {
                             div { class: "control",
                                 div { class: "select is-fullwidth",
                                     select {
-                                        for product in editor.products.iter() {
-                                            option { "{product}" }
+                                        onchange: move |e| product_state.set(e.value().clone()),
+                                        for product in products.iter() {
+                                            option {
+                                                value: "{product}",
+                                                selected: product == &product_state(),
+                                                "{product}"
+                                            }
                                         }
                                     }
                                 }
@@ -93,7 +118,7 @@ fn EditLot(editor: Editor, on_end: EventHandler<Ending>) -> Element {
                         div { class: "field column is-half",
                             label { class: "label", "Quantity" }
                             div { class: "control",
-                                input { class:"input", type: "number" }
+                                input { class:"input", type: "number", oninput: move |e| quantity_state.set(e.value()) }
                             }
                         }
                     }
